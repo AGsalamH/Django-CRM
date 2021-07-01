a5c7161a385ae0c94b21607bba859fe4dd16ac65
Ahmed Gamal <35201159+AGsalamH@users.noreply.github.com>
2021-07-01 18:56:04 +0200

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -22,9 +22,9 @@ class Product(models.Model):
     
     name = models.CharField(max_length = 200)
     price = models.DecimalField(decimal_places=2, max_digits=10)
-    category = models.CharField(max_length=50, choices=CATEGORY, null=True)
-    date_created = models.DateTimeField(auto_now_add=False, null=True, default=timezone.now)
-    description = models.CharField(max_length=300, null=True)
+    category = models.CharField(max_length=50, choices=CATEGORY, blank=True)
+    date_created = models.DateTimeField(null=True, default=timezone.now)
+    description = models.CharField(max_length=300, blank=True)
 
     def __str__(self):
         return self.name
@@ -36,10 +36,10 @@ class Order(models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivered', 'Delivered'),
     )
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
-    status = models.CharField(max_length=50, choices=STATUS, null=True, default='Pending')
-    date_created = models.DateTimeField(auto_now_add=False, null=True, default=timezone.now)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50, choices=STATUS, default='Pending')
+    date_created = models.DateTimeField(null=True, default=timezone.now)
 
     def __str__(self):
         return f'{self.customer}\'s Order'
